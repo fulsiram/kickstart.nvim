4828bff0065c915574479f8ab972aa8d1b82e058
Oleksandr Derevianko <fulsiram@riseup.net>
2025-06-23 03:29:04 -0400

    Add leader yank and delete mappings

--- init.lua
+++ init.lua
@@ -172,6 +172,11 @@ vim.o.confirm = true
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>y', [["+y]])
+
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
