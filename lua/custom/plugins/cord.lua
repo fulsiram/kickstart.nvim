81f794f70c68a5aeb195f585911549c3f4f69d50
Oleksandr Derevianko <fulsiram@riseup.net>
2025-07-02 21:32:01 -0400

    Sanitize filenames in cord.nvim

--- lua/custom/plugins/cord.lua
+++ lua/custom/plugins/cord.lua
@@ -1,4 +1,19 @@
+local sanitize_filename = function(filename)
+  return filename:gsub('^[^.]*', 'NDA')
+end
+
 return {
   'vyfor/cord.nvim',
   build = ':Cord update',
+  opts = {
+    text = {
+      workspace = '',
+      viewing = function(opts)
+        return string.format('Viewing %s', sanitize_filename(opts.filename))
+      end,
+      editing = function(opts)
+        return string.format('Editing %s', sanitize_filename(opts.filename))
+      end,
+    },
+  },
 }
